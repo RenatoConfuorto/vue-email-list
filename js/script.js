69d0593c1467ba958f76d13789049413c29817a2
RenatoConfuorto <renato.confuorto@outlook.it>
2022-05-14 11:58:29 +0200

    Bonus

--- js/script.js
+++ js/script.js
@@ -3,18 +3,33 @@ const app = new Vue({
   data: {
     emailNumber: 10,
     emails: [],
+    arrayCompleted: false,
   },
+
+  methods: {
+
+    showEmails(){
+      if(this.emails.length == this.emailNumber){
+        this.arrayCompleted = true;
+      }
+    },
+
+  },
+
   created(){
     for(let i = 0; i < this.emailNumber; i++){
       axios
         .get('https://flynn.boolean.careers/exercises/api/random/mail')
         .then((response) => {
-          // this.emails.push(response.data.response);
+
           this.emails.push(response.data.response);
           console.log(response.data.response);
+
+          this.showEmails();
         });
     }
   },
+
 });
 
 
